b076bb4baeedf7e6e1501e48dca79bceeaaf44c4
odai <odeartanan@gmail.com>
2025-05-22 20:12:07 +0300

    ff

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
 import app from "../app.js";
 import serverless from "serverless-http";
 
-export const handler = serverless(app);
+export default serverless(app); // ✅ Export default بدلاً من export const
