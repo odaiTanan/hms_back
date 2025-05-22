f588d1b918b3a82adce82c134822a37e0dc41e80
odai <odeartanan@gmail.com>
2025-05-22 21:41:27 +0300

    gG

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 import app from "../app.js";
 import serverless from "serverless-http";
 import { dbConnection } from "../database/dbConnection.js";
-await dbConnection(); // الاتصال مرة واحدة فقط قبل تغليف السيرفر
+//await dbConnection(); // الاتصال مرة واحدة فقط قبل تغليف السيرفر
 export default serverless(app); // ✅ Export default بدلاً من export const
